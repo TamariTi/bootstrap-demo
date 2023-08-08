e21a063e4d27ed38e40ae6b27373a3c0013a74dd
dsl2022 <2015rpro@gmail.com>
2023-08-07 21:38:40 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavSearchBar from './components/NavBar'
+import NavSearchBar from './components/NavBar';
+import {musicData} from './Data'
+import MusicCard from './components/MusicCard'
+
+function RenderData(){
+  return musicData.map(music=><MusicCard 
+    artist={music.artist} 
+    genre={music.genre}  
+    albumName={music.albumName} 
+    year={music.yearPublished} />)
+}
+
 function App() {
+  
   return (
     <div className="App">
       <NavSearchBar/>
+      <ul>
+        <RenderData/>
+      </ul>
     </div>
   );
 }
---
+++ src/components/MusicCard/index.js
@@ -0,0 +1,22 @@
+import Card from 'react-bootstrap/Card';
+
+function MusicCard({artist,genre,year,albumName}) {
+    console.log({artist,genre,year,albumName})
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{albumName}</Card.Title>
+        <Card.Text className="mb-2 text-muted">{artist}</Card.Text>
+        <Card.Text>
+          {genre}
+        </Card.Text>
+        <Card.Text>
+          {year}
+        </Card.Text>
+        <Card.Link href="#">demo</Card.Link>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default MusicCard;
